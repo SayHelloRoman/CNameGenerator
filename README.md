d7145d2c82b9e56f674e22517e66daced48a4165
Roman <68429513+SayHelloRoman@users.noreply.github.com>
2021-07-13 22:35:24 +0300

    CNameGenerator

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# CNameGenerator
- NameGenerator on C
+# C name generator
+
+## Что это?
+
+- Это просто генератор имён на Си
+
+## Информация
+
+- компилятор - gcc 9.2.0
+
+## Скриншоты
+
+<p align="center">
+  <img src="https://cdn.discordapp.com/attachments/857353656122998824/864588947924189264/unknown.png"/>
+</p>
---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <time.h>
+
+
+int main() {
+    srand(time(NULL));
+    uint8_t number_letters;
+    printf("Enter the number of characters: ");
+    scanf("%d", &number_letters);
+
+    if(number_letters <= 0) {
+        printf("The number cannot be less than or equal to zero\n");
+        return 1;
+    }
+
+    char vowels[] = "qwrtpsdfghjlkzxcvbnm";
+    char consonants[] = "aeyuio";
+
+    for(int i = 0;  i < number_letters / 2; i++){
+        printf("%c%c", vowels[rand() % 20], consonants[rand() % 6]);
+    }
+
+    if(number_letters % 2 != 0){
+        printf("%c", vowels[rand() % 20]);
+    }
+    
+    return 0;
+}
